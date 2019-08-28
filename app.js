8d679e8b70f040be660aae4de18869b3854aa551
kemmerle <kemmerle@umich.edu>
2019-08-28 11:40:30 -0400

    Added all thumbnail images for projects; got errors working and created an error template; personalized css of website

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ app.set('view engine', 'pug');
 const mainRoutes = require('./routes');
 app.use(mainRoutes);
 
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.render('error', err);
+});
+
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
 });
